dede524293c5c1d039aa124a4a2e7ce839427b4d
bakar-andguladze <baqar.andghuladze@gmail.com>
2025-11-30 15:01:11 +0100

    add solution to findFactorial

--- src/05_logic_control_flow/findFactorial.js
+++ src/05_logic_control_flow/findFactorial.js
@@ -1,13 +1,24 @@
-// Write a function that takes in a number and returns the factorial of that number.
-// A factorial is the product of an integer and all the integers below it.
-// For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
-// The factorial of 0 is 1.
-// The factorial of 1 is 1.
-// The factorial of 2 is 2 * 1 = 2.
-// The factorial of 3 is 3 * 2 * 1 = 6.
-// Recommended: use a while of for loop to solve the problem
+/*
+    Write a function that takes in a number and returns the factorial of that number.
+    A factorial is the product of an integer and all the integers below it.
+    For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+    The factorial of 0 is 1.
+    The factorial of 1 is 1.
+    The factorial of 2 is 2 * 1 = 2.
+    The factorial of 3 is 3 * 2 * 1 = 6.
+    Recommended: use a while or for loop to solve the problem
+*/
 function findFactorial(n) {
-    // Your code here
+    if(n === 0 || n === 1) {
+        return 1;
+    }
+
+    let factorial = 1;
+    for(let i = 2; i<= n; i++) {
+        factorial *= i;
+    }
+
+    return factorial;
 }
 
 module.exports = findFactorial;
